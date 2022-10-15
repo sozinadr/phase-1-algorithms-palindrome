00c23c1dd8003f3abdb3da42088f714508242e86
sozi <sozyarnadir@gmail.com>
2022-10-15 11:19:39 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+    // Write your algorithm here
+    let newWord = "";
+    for (let i = 0; i < word.length; i++) {
+        newWord += word[word.length - (i + 1)];
+    }
+    return newWord === word;
 }
 
 /* 
@@ -12,14 +17,14 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
